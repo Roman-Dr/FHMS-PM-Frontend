773f88c3e1e6ceba53678feb9f25e07bd961b137
dabruns <dbruns@web-computing.de>
2016-12-23 09:26:38 +0100

    add checkCredentialApi Method

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,8 +1,12 @@
-import {Injectable} from '@angular/core';
-import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http'
+import { User } from '../_models/user'
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/Rx'
 
-import {User} from '../_models/user'
-export {User}
+import {Router} from "@angular/router";
+import {sha1} from "@angular/compiler/src/i18n/digest";
+
 
 
 let user = new User;
@@ -18,16 +22,19 @@ let users = [
 @Injectable()
 export class AuthenticationService {
 
+  private _apiUrl = 'http://10.60.67.20:3000/api/users/';
+
 
   constructor(
-    private _router: Router){}
+    private _router: Router,
+  private http:Http){}
 
   logout() {
     localStorage.removeItem("user");
     this._router.navigate(['login']);
   }
 
-  login(user){
+  login(user: User){
 
     var authenticatedUser = users.find(u => u.firstName === user.firstName);
     if (authenticatedUser && authenticatedUser['birthDate'] === user.birthDate){
@@ -44,6 +51,15 @@ export class AuthenticationService {
       this._router.navigate(['login']);
     }
   }
+
+  checkCredentialsApi(user: User){
+    let toCheck = JSON.stringify({firstName: user.firstName,  birthDate: user.birthDate})
+    var encryptedCheck = sha1(toCheck)
+
+    return this.http.get(this._apiUrl + encryptedCheck)
+      .map(res => <User[]> res.json())
+  }
+
 
 
 
