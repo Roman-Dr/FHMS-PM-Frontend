93ab7f312a0200d45b4eece0ec90910a67bfd06c
dabruns <dbruns@web-computing.de>
2016-12-23 12:07:31 +0100

    fix url in user service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {UserService} from "../_services/user.service";
 import {User} from "../_models/user";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -14,16 +15,21 @@ export class RegisterComponent {
   newUser: User = new User()
   errorMessage: string;
 
-  constructor(private _userService: UserService) { }
-
-  ngOnInit() {
+  constructor(private _userService: UserService, private _router: Router) {
   }
 
+
   addUser() {
-    this._userService.addUser(this.newUser)
-      .subscribe(
+    var success = this._userService.addUser(this.newUser)
+    if (success) {
+      console.log(this._router);
+      success.subscribe(
         user => this.newUser = user,
         error => this.errorMessage = <any> error
-      )
+      );
+      this._router.navigate(['landing']);
+    } else {
+      console.log("Register failed, display error to user");
+    }
   }
 }
