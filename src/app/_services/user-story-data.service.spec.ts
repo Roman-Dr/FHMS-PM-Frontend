183076d820442d1deadc708ba10739b6e66b7b9a
TheChosenOne1 <sroelfes1@gmail.com>
2016-12-23 15:05:02 +0100

    clean up the code

--- src/app/_services/user-story-data.service.spec.ts
+++ src/app/_services/user-story-data.service.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { UserStoryDataService } from './user-story-data-service';
+import { UserStoryDataService } from './user-story-data.service';
 
 describe('UserStoryDataService', () => {
   beforeEach(() => {
