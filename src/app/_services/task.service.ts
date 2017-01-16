8f5a3f0287b2d5441a6b73257d2c4618f209a4dc
TheChosenOne1 <sroelfes1@gmail.com>
2017-01-17 00:46:25 +0100

    Implemented removing task function. This function is missing on the Server!

--- src/app/_services/task.service.ts
+++ src/app/_services/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http'
+import {Http, Response, Headers} from '@angular/http'
 import 'rxjs/Rx'
+import {Observable} from "rxjs";
 
 
 @Injectable()
@@ -8,6 +9,8 @@ export class TaskService {
 
 
   private _apiUrl = 'http://10.60.67.20:3000/api/projects/';
+  private backlogitemsUrl = sessionStorage.getItem('project_url')+'/backlogitems';
+  private headers: Headers = new Headers({ 'Content-Type': 'application/json' });
   private project_id= sessionStorage.getItem('project_url');
   // private _apiUrl = 'http://localhost:3000/api/user/';
 
@@ -19,11 +22,28 @@ export class TaskService {
       .map(res => res.json())
   }
 
- /* deleteBacklogitemTask(task_id, backlogitem_id){
+  deleteTask(backlogitem_id, task_id){
     return this.http.delete(this.backlogitemsUrl+"/"+backlogitem_id+"/tasks/"+task_id,{headers: this.headers})
       .map(this.extractData)
       .catch(this.handleErrorDelete);
-  }*/
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+
+  private handleErrorDelete(error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
+
+
+  /* deleteBacklogitemTask(task_id, backlogitem_id){
+     return this.http.delete(this.backlogitemsUrl+"/"+backlogitem_id+"/tasks/"+task_id,{headers: this.headers})
+       .map(this.extractData)
+       .catch(this.handleErrorDelete);
+   }*/
 }
 
 
